ebaa520fafe3b1510de8625843955d0dea692799
Daniel <danielptm@me.com>
2019-12-26 20:21:49 -0800

    Got terraform plan to work with reading in credentials from local file

--- resources.tf
+++ resources.tf
@@ -1,19 +1,21 @@
 provider "aws" {
   region                  = "us-east-1"
-  shared_credentials_file = "/Users/danieltuttle/.tf_user/creds/accessKeys.csv"
+  shared_credentials_file = "/Users/danieltuttle/.aws/credentials"
+  profile                 = "default"
 }
 
 resource "aws_instance" "example" {
-  ami           = var.AMI
+  ami           = "ami-13be557e"
   instance_type = "t2.small"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = var.BUCKET
-  acl    = "private"
-
-  tags = {
-    Name        = var.BUCKET
-    Environment = "Dev"
-  }
-}
+#
+# resource "aws_s3_bucket" "b" {
+#   bucket = var.BUCKET
+#   acl    = "private"
+#
+#   tags = {
+#     Name        = var.BUCKET
+#     Environment = "Dev"
+#   }
+# }
--- vars.tf
+++ vars.tf
@@ -1,13 +1,13 @@
-variable "BUCKET" {
-  type    = string
-  default = "MY_BUCKET"
-}
-
-variable "AWS_REGION" {
-  default = "us-west-1"
-}
-
-variable "AMI" {
-  type    = string
-  default = "ami-13be557e"
-}
+# variable "BUCKET" {
+#   type    = string
+#   default = "MY_BUCKET"
+# }
+#
+# variable "AWS_REGION" {
+#   default = "us-west-1"
+# }
+#
+# variable "AMI" {
+#   type    = string
+#   default = "ami-13be557e"
+# }
